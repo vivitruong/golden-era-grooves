bad80cb7e23a538395c90154e1a68dede5882519
vivitruong <vivitruong1506@gmail.com>
2023-09-26 19:41:17 -0500

    check paint

---
+++ react-app/src/components/Left/Paint/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+function Paint({ onClose, isFocus }) {
+    return (
+      <div
+        style={{
+          width: '100%',
+          height: '100%',
+          position: 'relative',
+        }}
+      >
+        <iframe
+          src="https://jspaint.app"
+          frameBorder="0"
+          title="paint"
+          style={{
+            display: 'block',
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'rgb(192,192,192)',
+          }}
+        />
+        {!isFocus && (
+          <div
+            style={{
+              width: '100%',
+              height: '100%',
+              position: 'absolute',
+              left: 0,
+              top: 0,
+            }}
+          />
+        )}
+      </div>
+    );
+  }
+
+  export default Paint;
