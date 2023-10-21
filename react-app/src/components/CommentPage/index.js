0806682451dc1cf8921baf14163a5897f62fe780
vivitruong <vivitruong1506@gmail.com>
2023-10-21 16:17:51 -0500

    uncommented out the like

--- react-app/src/components/CommentPage/index.js
+++ react-app/src/components/CommentPage/index.js
@@ -4,19 +4,17 @@ import Divider from "../Divider";
 
 
 const CommentPage = () => {
-  // const { likedSong } = useSelector((state) => state.likedSongs);
+  const { likedSong } = useSelector((state) => state.likedSongs);
 
   return (
     <div style={{ width: "100%" }}>
-      {/* <PlayListHeader /> */}
+      <PlayListHeader />
       <Divider />
       <ActionBar />
       <Divider />
-      {/* {likedSong.length === 0 ? ( */}
       <h4>Comment Feature Coming Soon! Please stay tune with us 🤗</h4>
-      {/* ) : ( */}
-      {/* <Songs songs={likedSong} /> */}
-      {/* )} */}
+      {likedSong.length === 0 ? null : <Songs songs={likedSong} />}
+
     </div>
   );
 };
