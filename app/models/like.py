e11da49ae3f7a5519c5f94594e5b374292faada1
vivitruong <vivitruong1506@gmail.com>
2023-11-06 14:12:32 -0600

    change some backend for like

--- app/models/like.py
+++ app/models/like.py
@@ -1,9 +1,23 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from datetime import datetime
 
-likes = db.Table(
-    'likes',
-    db.Model.metadata,
-    db.Column('users', db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), primary_key=True ),
-    db.Column('songs', db.Integer, db.ForeignKey(add_prefix_for_prod('songs.id')), primary_key=True )
-)
+class Like(db.Model):
+    __tablename__ = 'likes'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    song_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('songs.id')), nullable=False)
+
+    user = db.relationship('User', back_populates='likes')
+    song = db.relationship('Song', back_populates='likes')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'song_id': self.song_id,
+            'song': self.song.to_dict()
+        }
