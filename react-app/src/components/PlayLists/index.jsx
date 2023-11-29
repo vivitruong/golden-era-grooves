ecba9cbbb4996230e1dc4e0631ebb22e3fd9bebe
vivitruong <vivitruong1506@gmail.com>
2023-11-28 21:40:00 -0600

    fixing some minor code to look clean

--- react-app/src/components/PlayLists/index.jsx
+++ react-app/src/components/PlayLists/index.jsx
@@ -1,20 +1,13 @@
-// import { useState } from "react";
-// import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-// import { getSongs } from "../../slices/songsSlice";
 import "./style.css";
 import { currentPlayListSongs } from "../../store/slices/playlistSlice";
 import { useHistory } from "react-router-dom";
-// import { useGetCurrentUserPlayListQuery } from "../../slices/playListApiSlice";
+
 
 const PlayLists = () => {
-  // const { data: usersPlaylist } = useGetCurrentUserPlayListQuery();
   const dispatch = useDispatch();
   const history = useHistory();
-
   const playlists = useSelector((state) => state.userPlaylists);
-  // const navigate = useNavigate();
-
   const handlePlaylistClick = (playlist) => {
 
     dispatch(currentPlayListSongs({ songs: playlist?.playlist_songs }));
@@ -25,7 +18,6 @@ const PlayLists = () => {
     return (
       <div
         key={index}
-        // className={`playListName ${isActive ? "active" : ""}`}
         className={`playListName `}
         onClick={() => handlePlaylistClick(playlist)}
       >
